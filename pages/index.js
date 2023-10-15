188651717e8c190062b0a9f75b43b24faec885ad
Layan Mahendra <layanmahendra@Layans-MacBook-Air.local>
2023-10-15 11:56:37 +0530

    optional updates2

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ export default function Home({products}) {
                 {/* heading and paragaph start */}
                 <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-                        Cushioning for Your Miles
+                    Spice up your life, one click at a time
                     </div>
                     <div className="text-md md:text-xl">
-                        A lightweight Nike ZoomX midsole is combined with
-                        increased stack heights to help provide cushioning
-                        during extended stretches of running.
+                    Discover a world of flavor at our spices e-commerce store. 
+                    Elevate your culinary creations with our premium, 
+                    hand-selected spices delivered right to your doorstep.
                     </div>
                 </div>
                 {/* heading and paragaph end */}
