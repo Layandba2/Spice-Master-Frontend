838427d4d31dde13d0ed78aea19a7d2e4dfdb5b9
Layan Mahendra <layanmahendra@Layans-MacBook-Air.local>
2023-10-15 11:59:45 +0530

    optional updates2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                             send us feedback
                         </div>
                         <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
-                            student discount
+                            Bulk Orders
                         </div>
                     </div>
                     {/* MENU END */}
@@ -57,7 +57,7 @@ const Footer = () => {
                         {/* MENU START */}
                         <div className="flex flex-col gap-3">
                             <div className="font-oswald font-medium uppercase text-sm">
-                                About nike
+                                About Spices Master
                             </div>
                             <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
                                 News
@@ -106,7 +106,7 @@ const Footer = () => {
             <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
                 {/* LEFT START */}
                 <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
-                    © 2023 Nike, Inc. All Rights Reserved
+                    © 2023 Spices Master, Inc. All Rights Reserved
                 </div>
                 {/* LEFT END */}
 
